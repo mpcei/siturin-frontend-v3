9e81ad61f1fad0894f272e8e763771cfd22566a0
cmtl <cesar.tamayo0204@gmail.com>
2026-06-08 14:57:39 -0500

    fix bugs env qa

--- src/environments/environment.qa.ts
+++ src/environments/environment.qa.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
-    APP_URL: 'https://qa-siturin.produccion.gob.ec',
+    APP_URL: 'https://siturin-qa.produccion.gob.ec',
     APP_SHORT_NAME: 'SITURIN',
     APP_NAME: 'Sistema de Turismo Inteligente',
-    APP_PATH_ASSETS: 'production',
+    APP_PATH_ASSETS: 'development',
     APP_VERSION: '3.0.0',
-    API_URL: 'https://qa-api-siturin.produccion.gob.ec/api/v1',
-    API_ASSETS: 'https://qa-api-siturin.produccion.gob.ec/assets'
+    API_URL: 'https://api-siturin-qa.produccion.gob.ec/api/v1',
+    API_ASSETS: 'https://api-siturin-qa.produccion.gob.ec/assets'
 };
